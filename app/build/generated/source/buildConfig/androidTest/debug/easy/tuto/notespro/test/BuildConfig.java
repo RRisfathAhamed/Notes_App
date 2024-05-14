4841196dad5e8b8279ee159c2fdea8ae24db0a54
Risfath Ahamed <87221594+RRisfathAhamed@users.noreply.github.com>
2024-05-14 23:17:18 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/easy/tuto/notespro/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package easy.tuto.notespro.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "easy.tuto.notespro.test";
+  public static final String BUILD_TYPE = "debug";
+}
